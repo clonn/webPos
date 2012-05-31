008e36337660ca478a9e676503b0295a5e7a968a
Caesar Chi <clonncd@gmail.com>
2012-05-31 08:50:29 +0800

    added facebook login.

--- app.js
+++ app.js
@@ -3,13 +3,36 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , config  = require('./config.ini')
-  , routes = require('./routes')
-  , mongoose = require('mongoose');
+var express = require('express'),
+    config  = require('./config.ini'),
+    routes = require('./routes'),
+    mongoose = require('mongoose'),
+    passport = require('passport'),
+    util = require('util'),
+    FacebookStrategy = require('passport-facebook').Strategy;
+
+// set pasport
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+passport.use(new FacebookStrategy({
+    clientID: config.facebook.appid,
+    clientSecret: config.facebook.secret,
+    callbackURL: "http://localhost:3000/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    process.nextTick(function () {
+      return done(null, profile);
+    });
+  }
+));
+
 
 // Setup MongoDB
-
 var Schema = mongoose.Schema
   , ObjectId = Schema.ObjectId;
 
@@ -35,8 +58,13 @@ var app = module.exports = express.createServer();
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
+  app.use(express.logger());
+  app.use(express.cookieParser());
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(express.session({ secret: 'keyboard cat' }));
+  app.use(passport.initialize());
+  app.use(passport.session());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
@@ -50,8 +78,44 @@ app.configure('production', function(){
 });
 
 // Routes
+app.get('/', function(req, res){
+  res.render('index', { user: req.user });
+});
 
-app.get('/', routes.index);
+app.get('/account', ensureAuthenticated, function(req, res){
+  res.render('account', { user: req.user });
+});
+
+app.get('/login', function(req, res){
+  res.render('login', { user: req.user });
+});
+
+app.get('/chat', function(req, res){
+  res.render('chat', {title: 'this is a chat room', user: req.user });
+});
+
+app.get('/auth/facebook',
+  passport.authenticate('facebook'),
+  function(req, res){
+    // The request will be redirected to Facebook for authentication, so this
+    // function will not be called.
+});
+
+app.get('/auth/facebook/callback', 
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+});
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
 
 app.listen(process.env.PORT || process.env.VCAP_APP_PORT || process.env.npm_package_config_port || 3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,3 @@
 /*
  * GET home page.
  */
-
-exports.index = function(req, res){
-  res.render('index', { title: 'Web position for Node.js Taiwan Party' })
-};
